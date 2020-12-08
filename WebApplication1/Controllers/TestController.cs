9cbac96c2db22bfb2102dbb119da2fa5e1d9452d
amir <amyrhesary32@gmail.com>
2020-12-08 21:51:53 +0330

    First commit

---
+++ WebApplication1/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class TestController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        // GET: Test
+        public ActionResult test1()
+        {
+            return View();
+        }
+        public ActionResult test2()
+        {
+            return View();
+        }
+    }
+}
